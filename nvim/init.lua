af8d6c233df2b44ddb878f10fb728ec956113d0d
stickynotememo <a@a.com>
2025-07-18 11:35:31 +1000

    Change vim colorscheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -904,7 +904,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'vscode'
+      vim.cmd.colorscheme 'torte'
     end,
   },
 
